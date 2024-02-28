59b3c965454bdcef691e60fad0ecfc1a69d7c950
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2024-02-28 22:02:33 +0800

    Updated button colours and added text to the buttons

--- GUI.py
+++ GUI.py
@@ -2,9 +2,9 @@ import PySimpleGUI as sg
 
 
 #This code is written by AI. It is kind of average. Instead of persisting state via a list, I would like to have the squares backed by soem data object I can reference.
-def main():
+def main(word: str):
     layout = [
-        [sg.Button('', size=(5, 2), key=f'square{i}', button_color=('gray', 'gray')) for i in range(5)],
+        [sg.Button(word[i], size=(5, 2), key=f'square{i}', font=('Helvetica', 32, 'normal'), button_color=('black', 'gray')) for i in range(5)],
         [sg.Button('Exit')]
     ]
 
@@ -23,8 +23,8 @@ def main():
             current_color_index = color_sequence.index(square_colors[square_index])
             new_color_index = (current_color_index + 1) % len(color_sequence)
             square_colors[square_index] = color_sequence[new_color_index]
-            window[event].update(button_color=(square_colors[square_index], square_colors[square_index]))
+            window[event].update(button_color=(square_colors[square_index]))
     window.close()
 
 if __name__ == '__main__':
-    main()
+    main("joker")
