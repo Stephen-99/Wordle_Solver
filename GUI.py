dc35afc00ef61b66dec35dd11cca4cc96c9aa126
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2024-03-01 20:49:42 +0800

    Some refactoring

--- GUI.py
+++ GUI.py
@@ -4,24 +4,27 @@ yellow = '#a39529'
 gray = '#424242'
 green = '#459824'
 
+#TODO return the results somehow and interact with Guess.py
+
 #This code is written by AI. It is kind of average. Instead of persisting state via a list, I would like to have the squares backed by soem data object I can reference.
-def main(word: str):
-    word = word.upper()
+def obtainGuessResults(guess: str):
+    word = guess.upper()
+    #display a square button for each letter, with the letter inside that button
     layout = [
-        #TODO: make it look more like the wordle theme colours.
         [sg.Button(word[i], size=(5, 2), key=f'square{i}', font=('Helvetica', 32, 'bold'), button_color=('white', gray)) for i in range(5)],
-        [sg.Button('Exit')]
+        [sg.Button('Submit')]
     ]
 
     window = sg.Window('Colorful Squares', layout)
 
+    #on click will change from gray -> yellow -> green -> gray...
     color_sequence = [gray, yellow, green]
     square_colors = [gray] * 5  # Initialize all squares to gray
 
     while True:
         event, values = window.read()
 
-        if event == sg.WINDOW_CLOSED or event == 'Exit':
+        if event == sg.WINDOW_CLOSED or event == 'Submit':
             break
         elif event.startswith('square'):
             square_index = int(event[len('square'):])
@@ -32,4 +35,4 @@ def main(word: str):
     window.close()
 
 if __name__ == '__main__':
-    main("joker")
+    obtainGuessResults("joker")
