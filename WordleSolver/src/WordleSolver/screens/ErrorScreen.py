309dc6bf7e62bd7340c888303204a37211ebaff3
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2024-06-25 22:37:05 +0800

    Updating error screen

--- WordleSolver/src/WordleSolver/screens/ErrorScreen.py
+++ WordleSolver/src/WordleSolver/screens/ErrorScreen.py
@@ -11,29 +11,33 @@ from WordleSolver.Events.Events import ReturnToMainMenuEvent
 class ErrorScreen(Screen):
     def __init__(self):
         self.errorMsg = None
-        self.box = None
+        self.innerBox = None
+        self.outerBox = None
         self.text = None
         self.okButton = None
 
     def CreateScreen(self):
-        self.box = toga.Box(style=Pack(direction=COLUMN, alignment="center"))
-        self.text = toga.Label(self.errorMsg, style=Pack(padding=(2,5), font_size=16, text_align='center'))
-
-        #TODO: restrict the width fo this button (just needs to fit the text really)
-        self.okButton = toga.Button("Ok", on_press=self.OkButtonHandler, style=Pack(padding=5, font_size=12))
+        self.outerBox = toga.Box(style=Pack(direction=ROW, alignment="center"))
+        self.innerBox = toga.Box(style=Pack(direction=COLUMN, alignment="center", flex=1))
+        self.CreateTextLabel()
+        self.okButton = toga.Button("Ok", on_press=self.OkButtonHandler, style=Pack(padding=5, font_size=12, width=80))
         
-        self.box.add(self.text)
-        self.box.add(self.okButton)
-    
+        self.innerBox.add(self.text)
+        self.innerBox.add(self.okButton)
+        self.outerBox.add(self.innerBox)
+
     def UpdateMessage(self, msg):
         self.errorMsg = msg
 
     def UpdateScreen(self):
-        self.box.remove(self.text)
-        self.text = toga.Label(self.errorMsg, style=Pack(padding=(2,5), font_size=16, text_align='center'))
-        self.box.insert(0, self.text)
+        self.innerBox.remove(self.text)
+        self.CreateTextLabel()
+        self.innerBox.insert(0, self.text)
 
-        return self.box
+        return self.outerBox
+
+    def CreateTextLabel(self):
+        self.text = toga.Label(self.errorMsg, style=Pack(padding=(2,5), font_size=16, text_align='center'))
 
     def OkButtonHandler(self, widget):
         EventSystem.EventOccured(ReturnToMainMenuEvent())
