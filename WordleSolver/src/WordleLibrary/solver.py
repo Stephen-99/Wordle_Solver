1b76a4eb010ea41d057d9e7651895a0f840e9c15
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2024-06-28 23:54:18 +0800

    cleaned up old functions in solver

--- WordleSolver/src/WordleLibrary/solver.py
+++ WordleSolver/src/WordleLibrary/solver.py
@@ -144,6 +144,8 @@ class WordleSolver:
         self.lookup = CharCommonality(self.validWords)
 
 
+#TODO: put these functions in the class where it makes sense, otherwise put them in a sepearate file for static functions
+
 def PlayWordle():
     db = WordleDB()
     words, allowedWords = db.GetWords()
@@ -187,93 +189,28 @@ def RunWithoutGUI():
     print("Took ", RunGame(words, commonalityLookup, TESTWORD, db), "guesses")
 
 
-def RunWithUserInput():
-    db = WordleDB()
-    words, allowedWords = db.GetWords()
-    commonalityLookup = DetermineNumberOfOccurrences(words)
-    SolveFromUser(words, commonalityLookup, db)
+# converts them from a per word basis to a list of letter combinations.
+# GetLetterCombinations can then take these and convert them to combinations based on the number of required letters
+# like a 5 choose 3 type situation.
+def ProcessLeastCommonLetters(LCLetters: list[list[str]]) -> list[list[str]]:
+    letterCombs = []
+    for letter in LCLetters[0]:
+        if len(LCLetters) > 1:
+            combs = ProcessLeastCommonLetters(LCLetters[1:])
+            for comb in combs:
+                comb.append(letter)
+                letterCombs.append(comb)
+        else:
+            letterCombs.append([letter])
+    return letterCombs
 
 
-#--> --> --> DEPRECEATED <-- <-- <--
-def SolveFromUser(
-    validWords: list[str], commonalityLookup: CharCommonality, db: WordleDB
-) -> int:
-    try:
-        guess, score = DetermineGuess(commonalityLookup, validWords, db)
-    except InvalidWordLength as e:
-        print(e.message)
-        return
-    except IndexError as err:
-        print("Invalid selection. There are no valid words left.")
-        return
+def GetLetterCombinations(letters: list[str], maxLetters: int) -> list[list[str]]:
+    if len(letters) >= maxLetters:
+        return LetterCombinations(letters, maxLetters)
 
-    #    print("Best guess is:", guess.word, " With a score of:", score)
-    guiGuessResults = GUI.ObtainGuessResults(guess.word)
-    if not guiGuessResults:
-        return
-
-    correctGuess = guess.UserValidateGuess(guiGuessResults)
-
-    guesses = 1
-
-    while not correctGuess:
-        validWords = FilterWords(validWords, guess)
-        # print("VALID WORDS:", validWords)
-        commonalityLookup = DetermineNumberOfOccurrences(validWords)
-        try:
-            guess, score = DetermineGuess(commonalityLookup, validWords, db)
-        except InvalidWordLength as e:
-            print(e.message)
-            return
-        except IndexError as err:
-            print("Invalid selection. There are no valid words left.")
-            return
-        #        print("Best guess is:", guess.word, " With a score of:", score)
-        guiGuessResults = GUI.ObtainGuessResults(guess.word)
-        if not guiGuessResults:
-            return
-
-        correctGuess = guess.UserValidateGuess(guiGuessResults)
-
-        guesses += 1
-
-    return guesses
-
-#--> --> --> DEPRECEATED <-- <-- <--
-def DetermineNumberOfOccurrences(words: list[str]) -> CharCommonality:
-    charsLookup = CharCommonality()
-    charsLookup.AddCommonality(words)
-    return charsLookup
-
-#--> --> --> DEPRECEATED <-- <-- <--
-def DetermineGuess(
-    commonalityLookup: CharCommonality, words: list[str], db: WordleDB
-) -> tuple[Guess, int]:
-    # TODO Won't need to return bestScore once all setup
-    bestScore = 0
-    bestWord = []
-    for word in words:
-        score = commonalityLookup.RetriveCommonality(word)
-        if score > bestScore:
-            bestScore = score
-            bestWord = [word]
-        elif score == bestScore:
-            bestWord.append(word)
-    print("bestGuesses:", bestWord, "    Remaining number of valid words:", len(words))
-
-    try:
-        if len(bestWord) >= 2 and 2 < len(words) < 7:
-            # a commonality score for this word won't make sense, as some of the letters might be missing
-            return Guess(PickVarietyWord(commonalityLookup, db, words, minLetters=2)), 0
-        if len(bestWord) >= 4 and 3 < len(words) < 11:
-            return Guess(PickVarietyWord(commonalityLookup, db, words, minLetters=4)), 0
-        if len(bestWord) >= 4 and 11 < len(words) < 20:
-            return Guess(PickVarietyWord(commonalityLookup, db, words, minLetters=5)), 0
-    except InvalidWordLength as e:
-        pass
-
-    return Guess(bestWord[0]), bestScore
-
+    # This will occur if there are less letters then the given maxLetters
+    return LetterCombinations(letters, len(letters))
 
 def LetterCombinations(letters: list[str], max: int = 5) -> list[list[str]]:
     if len(letters) == max:
@@ -299,67 +236,6 @@ def _LCR(
         del curLetters[-1]
     return combs
 
-#--> --> --> DEPRECEATED <-- <-- <--
-def PickVarietyWord(
-    lookup: CharCommonality, db: WordleDB, words: list[str], minLetters: int = 2
-) -> str:
-    # print("Looking for the least common letters:")
-    letters = lookup.GetLeastCommonLetters(words)
-
-    # print(letters)
-
-    if len(letters) < 2:
-        return "A variety word is not helpful here"
-
-    letterCombs = ProcessLeastCommonLetters(letters)
-
-    ii = min(5, len(letterCombs[0]))
-    varietyWord = None
-    while not varietyWord and ii >= minLetters:
-        varietyWord = TryGetVarietyWord(letterCombs, ii, db)
-        ii -= 1
-
-    # Should never trigger but you know how things go
-    if not varietyWord:
-        return "Non-standard word length"
-
-    return varietyWord["word"]
-
-
-# converts them from a per word basis to a list of letter combinations.
-# GetLetterCombinations can then take these and convert them to combinations based on the number of required letters
-# like a 5 choose 3 type situation.
-def ProcessLeastCommonLetters(LCLetters: list[list[str]]) -> list[list[str]]:
-    letterCombs = []
-    for letter in LCLetters[0]:
-        if len(LCLetters) > 1:
-            combs = ProcessLeastCommonLetters(LCLetters[1:])
-            for comb in combs:
-                comb.append(letter)
-                letterCombs.append(comb)
-        else:
-            letterCombs.append([letter])
-    return letterCombs
-
-
-def GetLetterCombinations(letters: list[str], maxLetters: int) -> list[list[str]]:
-    if len(letters) >= maxLetters:
-        return LetterCombinations(letters, maxLetters)
-
-    # This will occur if there are less letters then the given maxLetters
-    return LetterCombinations(letters, len(letters))
-
-#--> --> --> DEPRECEATED <-- <-- <--
-def TryGetVarietyWord(
-    lettersOptionsByWord: list[list[str]], maxLetters: int, db: WordleDB
-) -> str:
-    letterCombs = []
-    for letters in lettersOptionsByWord:
-        letterCombs.extend(GetLetterCombinations(letters, maxLetters))
-
-    filter = GetFilterForCombinations(letterCombs)
-    return db.FindOneAllowedWord(filter)
-
 
 def GetFilterForCombinations(combinations: list[list[str]]) -> str:
     if len(combinations) == 1:
@@ -384,47 +260,6 @@ def GetAllLetterFilter(letters: list[str]) -> str:
 
     return filter
 
-#--> --> --> DEPRECEATED <-- <-- <--
-def FilterWords(words: list[str], guess: Guess) -> list[str]:
-    return [word for word in words if guess.ConsistentWithGuess(word)]
-
 
 def GetRandomWord(words: list[str]) -> str:
     return random.choice(words)
-
-
-def RunGame(
-    validWords: list[str], commonalityLookup: CharCommonality, answer: str, db: WordleDB
-) -> int:
-    try:
-        guess, score = DetermineGuess(commonalityLookup, validWords, db)
-    except InvalidWordLength as e:
-        print(e.message)
-        return
-
-    #    print("Best guess is:", guess.word, " With a score of:", score)
-    correctGuess = guess.ValidateGuess(answer)
-    guesses = 1
-
-    while not correctGuess:
-        validWords = FilterWords(validWords, guess)
-        # print("VALID WORDS:", validWords)
-        commonalityLookup = DetermineNumberOfOccurrences(validWords)
-        try:
-            guess, score = DetermineGuess(commonalityLookup, validWords, db)
-        except InvalidWordLength as e:
-            print(e.message)
-            return
-        #        print("Best guess is:", guess.word, " With a score of:", score)
-        correctGuess = guess.ValidateGuess(answer)
-        guesses += 1
-
-    return guesses
-
-
-#OLD WORLD
-def main():
-    GUI.DisplayStartScreen()
-
-if __name__ == "__main__":
-    main()
