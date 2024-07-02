c16ea8184ef3d7ac55c15de22189403cfc15c866
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2024-07-02 23:01:48 +0800

    Initial play wordle screen

--- WordleSolver/src/WordleSolver/screens/PlayWordleScreen.py
+++ WordleSolver/src/WordleSolver/screens/PlayWordleScreen.py
@@ -6,15 +6,40 @@
         #have it fixed with the 6 rows, just keep the last ones blank for now
     #After a guess the next row will become active, and the one above will get colours.
 
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, ROW
+
+from WordleLibrary.LetterColour import LetterColour
+
 from .Screen import Screen
 
+#TODO add keyboard.
+    #Should auto pop up, but can add highlighting to show which letters are unavailable or should be used.
 class PlayWordleScreen(Screen):
     def __init__(self):
-        self.rows = None
-        self.curRow = None
+        self.outerBox = toga.Box(style=Pack(direction=ROW, alignment="center"))
+        self.innerBox = toga.Box(style=Pack(direction=COLUMN, alignment="center", flex=1))
+        self.rows = [self.CreateRow() for _ in range(6)]
+        self.curRow = self.rows[0]
+        self.curRowIdx = 0 #TODO: these 2 things make me thing I need a small class for rows.
 
     def CreateScreen(self):
-        pass
+        [self.innerBox.add(row) for row in self.rows]
+        #self.innerBox.add("SOME TEXT")
+        self.outerBox.add(self.innerBox)
+
     
     def UpdateScreen(self):
-        pass
+        return self.outerBox
+
+    def CreateRow(self):
+        squares = [self.CreateTextSquare() for _ in range(5)]
+        row = toga.Box(style=Pack(direction=ROW))
+        [row.add(square) for square in squares]
+
+        return row
+    
+    def CreateTextSquare(self):
+        size = 70
+        return toga.TextInput(style=Pack(padding=5, font_weight="bold", font_size=size//2, width=size, height=size, color="#ffffff", background_color=LetterColour.gray))
--- WordleSolver/src/WordleSolver/screens/ScreenManager.py
+++ WordleSolver/src/WordleSolver/screens/ScreenManager.py
@@ -2,6 +2,7 @@ from WordleSolver.screens.Screen import Screen
 from WordleSolver.screens.MainMenuScreen import MainMenuScreen
 from WordleSolver.screens.SolverScreen import SolverScreen
 from WordleSolver.screens.ErrorScreen import ErrorScreen
+from WordleSolver.screens.PlayWordleScreen import PlayWordleScreen
 
 from WordleSolver.Events import EventSystem
 from WordleSolver.Events.Events import NewWordEvent, ExitAppEvent, ReturnToMainMenuEvent, ErrorOccuredEvent
@@ -21,9 +22,12 @@ class ScreenManager:
         self.menuScreen.CreateScreen()
         self.errorScreen = ErrorScreen()
         self.errorScreen.CreateScreen()
+        self.playWordleScreen = PlayWordleScreen()
+        self.playWordleScreen.CreateScreen()
 
         #start with menuScreen (This may need to move to a separate startup func)
-        self.ChangeScreen(self.menuScreen)
+        #self.ChangeScreen(self.menuScreen)
+        self.ChangeScreen(self.playWordleScreen)
 
     def RegisterHandlers(self):
         EventSystem.subscribe(NewWordEvent, self.UpdateSolverScreen)
