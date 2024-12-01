fb413e0526980ec75b326e9fdad2a4ef77ce3249
Stephen-99 <56374726+Stephen-99@users.noreply.github.com>
2024-12-02 07:11:29 +0800

    re-built android

--- WordleSolver/src/WordleSolver/app.py
+++ WordleSolver/src/WordleSolver/app.py
@@ -39,6 +39,8 @@ def main():
     # Update windows msi to work with self-signed cert. ~~ It's not trusted anyway, and I don't want to pay for a cert, so not going to do this ~~
     # Take a 1024x500px feature graphic ~~ :D ~~
     # Course for publishing app ~~ :D ~~
+    # Self-sign the android app bundle
+    # Test on my phone
     # Create a store listing
 
 
